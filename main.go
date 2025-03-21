d9da651ea7611814b899e3d97cc722d8109a6c83
platosbeard <shahnawazsharief776@gmail.com>
2025-03-21 17:49:10 +0530

    refactor: Update main function to include interpreter and improve error handling

--- main.go
+++ main.go
@@ -1,57 +1,24 @@
 package main
 
 import (
+	"Crafting-interpreters/internal/interpreter"
 	"Crafting-interpreters/internal/parser"
-	"Crafting-interpreters/internal/printer"
-	"Crafting-interpreters/internal/repl"
 	"Crafting-interpreters/internal/scanner"
 	"fmt"
 )
 
 func main() {
-	// l := internal.Repl{}
-	// l.LoadProgram("./examples/program.txt")
-	programText := "(10*(4*5))"
-	scanner := scanner.TokenScanner{}
-	scanner.Init(programText)
-	_ = scanner.ScanTokens()
-	p := parser.Parser{Tokens: scanner.Tokens, Lox: repl.Repl{}}
-	expr, _ := p.Parse()
-	astPrinter := printer.PrintAST{}
-	astString := astPrinter.Print(expr)
-	fmt.Printf(astString)
-
-	//fmt.Println(scanner.Tokens)
-	//v1 := Literal{Value: 10}
-	//v2 := Literal{Value: 20}
-	//bin1 := Binary{Left: v1, Operator: "+", Right: v2}
-	//fmt.Println(bin1)
-	// err := internal.GenerateAst("internal/")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// MinusBinOp := internal.Binary{
-	// 	Left: internal.Literal{Value: 10},
-	// 	Operator: internal.Token{
-	// 		Type:    internal.MINUS,
-	// 		Lexeme:  "-",
-	// 		Literal: "-",
-	// 		Line:    1,
-	// 	},
-	// 	Right: internal.Literal{Value: 20},
-	// }
-	// AddBinOp := internal.Binary{
-	// 	Left: MinusBinOp,
-	// 	Operator: internal.Token{
-	// 		Type:    internal.PLUS,
-	// 		Lexeme:  "+",
-	// 		Literal: "+",
-	// 		Line:    1,
-	// 	},
-	// 	Right: internal.Literal{Value: 20},
-	// }
-	// printAst := internal.PrintAST{}
-	// str := AddBinOp.Accept(printAst)
-	// fmt.Println(str)
-
+	//l := repl.Repl{}
+	//l.LoadProgram("./examples/program.txt")
+	programText := "(10+(10*True))"
+	tokenScanner := scanner.TokenScanner{}
+	tokenScanner.Init(programText)
+	_ = tokenScanner.ScanTokens()
+	p := parser.Parser{Tokens: tokenScanner.Tokens}
+	expr, err := p.Parse()
+	if err != nil {
+		fmt.Println(err)
+	}
+	interpreter := interpreter.Interpreter{}
+	_ = interpreter.Interpret(expr)
 }
