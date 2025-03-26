d0f2952a797c009b0ac5938ae87d9b96143edc75
platosbeard <shahnawazsharief776@gmail.com>
2025-03-27 02:12:28 +0530

    Update main function to enhance expression evaluation and add AST printing

--- main.go
+++ main.go
@@ -3,22 +3,22 @@ package main
 import (
 	"Crafting-interpreters/internal/interpreter"
 	"Crafting-interpreters/internal/parser"
+	"Crafting-interpreters/internal/printer"
 	"Crafting-interpreters/internal/scanner"
 	"fmt"
 )
 
 func main() {
 	//l := repl.Repl{}
-	//l.LoadProgram("./examples/program.txt")
-	programText := "(10+(10*True))"
+	//l.LoadProgram("./examples/program.txt"
+	programText := "(10+(10 * 2 + (3 * 3)))"
 	tokenScanner := scanner.TokenScanner{}
 	tokenScanner.Init(programText)
 	_ = tokenScanner.ScanTokens()
 	p := parser.Parser{Tokens: tokenScanner.Tokens}
-	expr, err := p.Parse()
-	if err != nil {
-		fmt.Println(err)
-	}
+	expr, _ := p.Parse()
 	interpreter := interpreter.Interpreter{}
-	_ = interpreter.Interpret(expr)
+	interpreter.Interpret(expr, true)
+	printer := printer.PrintAST{}
+	fmt.Println("\n", printer.Print(expr))
 }
