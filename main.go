a4512cba3c80a4819356b6e8663911138c3b9f3c
platosbeard <shahnawazsharief776@gmail.com>
2025-07-29 13:41:43 +0530

    GI-8 Update main function to use explicit variable assignment and integrate interpreter

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/go-interpreter/internal/interpreter"
 	"github.com/go-interpreter/internal/parser"
 	"github.com/go-interpreter/internal/scanner"
 )
@@ -8,13 +9,13 @@ import (
 func main() {
 	//l := repl.Repl{}
 	//l.LoadProgram("./examples/program.txt"
-	programText := "var x = 10; x = 20;"
+	programText := "var x = (1 + 2 + (3+4));print x;"
 	tokenScanner := scanner.NewTokenScanner(programText)
 	_ = tokenScanner.ScanTokens()
 	p := parser.Parser{Tokens: tokenScanner.Tokens}
-	_ = p.Parse()
-	//inter := interpreter.NewInterpreter()
-	//inter.Interpret(expr)
+	expr := p.Parse()
+	inter := interpreter.NewInterpreter()
+	inter.Interpret(expr)
 	//printer := printer.PrintAST{}
 	//fmt.Println("\n", printer.Print(expr))
 }
