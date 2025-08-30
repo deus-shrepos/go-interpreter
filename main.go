bf591a73fdcffc4f35520be98c76d1d421f3e0e5
platosbeard <shahnawazsharief776@gmail.com>
2025-08-30 18:47:00 +0530

    GI-11 Implement REPL functionality and add control structures to example program

--- main.go
+++ main.go
@@ -1,36 +1,14 @@
 package main
 
 import (
-	"github.com/go-interpreter/internal/interpreter"
-	"github.com/go-interpreter/internal/parser"
-	"github.com/go-interpreter/internal/scanner"
+	// "fmt"
+
+	// "fmt"
+
+	"github.com/go-interpreter/internal/repl"
 )
 
 func main() {
-	// THE MAIN FILE WILL CHANGE
-	programText := `
-	var globalScope = 10;
-	  {
-	 	 var innerScope1 = 20;
-	     {
-			var innerScope2 = 30;
-	  		print innerScope2;
-			print "\n";
-	     }
-	     print innerScope1;
-		 print "\n";
-	  }
-	print globalScope;
-	`
-	tokenScanner := scanner.NewTokenScanner(programText)
-	_ = tokenScanner.ScanTokens()
-	p := parser.Parser{Tokens: tokenScanner.Tokens}
-	expr := p.Parse()
-	inter := interpreter.NewInterpreter()
-	err := inter.Interpret(expr)
-	if err != nil {
-		panic(err)
-	}
-	//printer := printer.PrintAST{}
-	//fmt.Println("\n", printer.Print(expr))
+	r := repl.NewRepl()
+	r.LoadProgram("examples/program.txt")
 }
