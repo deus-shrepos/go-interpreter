7996827bcb12632912b01efbba971ae51acb5ef8
platosbeard <shahnawazsharief776@gmail.com>
2025-10-31 19:09:15 +0530

    fix-and-add: add program loading from string and path, and minor fixes

--- internal/repl/repl.go
+++ internal/repl/repl.go
@@ -8,13 +8,14 @@ import (
 	// "github.com/go-interpreter/internal/interpreter"
 	// "github.com/go-interpreter/internal/interpreter"
 	"github.com/chzyer/readline"
+	"github.com/go-interpreter/internal/interpreter"
 	"github.com/go-interpreter/internal/printer"
 	"github.com/go-interpreter/internal/scanner"
 
 	parser "github.com/go-interpreter/internal/parser"
 )
 
-// TODO(ME): NEED TO MAKE BETTER. COMING SOON.
+// TODO(ME): LITERALLY CREATE A NICE REPL :)
 type Repl struct {
 	HadError bool
 }
@@ -24,31 +25,35 @@ func NewRepl() *Repl {
 
 }
 
-func (repl *Repl) LoadProgram(path string) {
-	if repl.HadError {
-		panic("Program had errors!")
-	}
-	err := repl.loadProgram(path)
-	if err != nil {
-		panic(err)
-	}
-}
-
 // Runfile We want to scan the tokens in a file
 // We want to scan correct tokens defined
 // in out hypothetical language
-func (repl *Repl) loadProgram(path string) error {
+func (repl *Repl) LoadProgramFromPath(path string, astOnly bool) {
+	if repl.HadError {
+		panic("Program execution stopped!")
+
+	}
 
 	// We are reading the program text here
 	file, err := os.ReadFile(path)
 	if err != nil {
 		_ = fmt.Errorf("an error occured during the program file read: %s", err)
-		return err
 	}
 	// We store that byte file for scanning
 	tokenScanner := scanner.NewTokenScanner(string(file))
+	if astOnly {
+		repl.PrintAST(&tokenScanner)
+	}
 	repl.run(&tokenScanner)
-	return nil
+}
+
+func (repl *Repl) LoadFromString(program string, astOnly bool) {
+	tokenScanner := scanner.NewTokenScanner(program)
+	if astOnly {
+		repl.PrintAST(&tokenScanner)
+		return
+	}
+	repl.run(&tokenScanner)
 }
 
 // This your token scanner for the program
@@ -56,27 +61,33 @@ func (repl *Repl) run(tokenScanner *scanner.TokenScanner) {
 	_ = tokenScanner.ScanTokens()
 	p := parser.NewParser(tokenScanner.Tokens)
 	parsedStatments := p.Parse()
-	printer := printer.PrintAST{}
-	printer.PrintAll(parsedStatments)
-	// inter := interpreter.NewInterpreter()
-	//err := inter.Interpret(parsedStatments)
-	//if err != nil {
-	//	repl.HadError = true
-	//	fmt.Println(err)
-	//}
-	//if repl.HadError {
-	//	return
-	//}
-	// astPrinter := printer.PrintAST{}
-	// astPrinter.Print(expr)
+	inter := interpreter.NewInterpreter()
+	err := inter.Interpret(parsedStatments)
+	if err != nil {
+		repl.HadError = true
+		fmt.Println(err)
+	}
+	if repl.HadError {
+		return
+	}
 }
 
+func (repl *Repl) PrintAST(tokenScanner *scanner.TokenScanner) {
+	_ = tokenScanner.ScanTokens()
+	p := parser.NewParser(tokenScanner.Tokens)
+	parsedStatments := p.Parse()
+	print := printer.PrintAST{}
+	print.PrintAll(parsedStatments)
+}
+
+// TODO: Fix the cli later when we have all the basic langauge constructs
 func (repl *Repl) RunCli() {
 	rl, err := readline.NewEx(
 		&readline.Config{
 			Prompt:                 ">>> ",
 			HistoryFile:            "/tmp/readline-multiline",
 			DisableAutoSaveHistory: true,
+			EOFPrompt:              "exit",
 		})
 	if err != nil {
 		panic(err)
